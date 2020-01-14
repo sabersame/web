3d174d95026596c75a793aadfd20995a35196084
saber_same <saber_same@foxmail.com>
2020-01-14 11:18:39 +0800

    init

---
+++ login/case_login.py
@@ -0,0 +1,47 @@
+from common.Base_Unit import Base_Base
+
+
+class Case_Login(Base_Base):
+    """
+    闻道作业测评测试的流程动作
+    """
+    def __init__(self):
+        super().__init__()
+
+    def start_operation(self, url):
+        # 开启浏览器
+        self.start(url)
+
+    def username_input_input(self, case_name, username):
+        # 用户名
+        self.input(case_name, 'xpath', '//*[@id="app-el"]/div/div/div/div[2]/div/div[2]/input', username)
+
+    def passwd_input_input(self, case_name, passwd):
+        # 密码
+        self.input(case_name, 'xpath', '//*[@id="app-el"]/div/div/div/div[2]/div/div[3]/input', passwd)
+
+    def signup_button_click(self, case_name):
+        # 登陆按钮
+        self.click(case_name, 'xpath', '//*[@id="app-el"]/div/div/div/div[2]/div/div[4]/button')
+
+    def userinfo_text_assertion(self, case_name):
+        self.assertion(case_name, 'xpath', '//*[@id="app-el"]/div/div/div/div/div[1]/div[3]/div/div/div', "狗狗")
+
+    def errorlogin_text_assertion(self, case_name):
+        self.assertion(case_name, 'xpath', '//*[@id="app-el"]/div/div/div/div[2]/div/div[4]/div', '密码错误!')
+
+    # 启动时日志记录
+    def start_log(self):
+        self.log(self.Set.operation_history_log, "info", "正在执行登录脚本......")
+
+    # 结束时日志记录
+    def end_log(self):
+        self.log(self.Set.operation_history_log, "info", "登陆脚本执行完毕.")
+
+    def sleep_operation(self, case_name):
+        # 暂停操作
+        self.delay(case_name)
+
+    def close_operation(self):
+        # 关闭浏览器
+        self.quit()
---
+++ login/test_login.py
@@ -0,0 +1,41 @@
+import pytest
+from case.case_login import Case_Login
+from public.Data_Unit import Data_Base
+
+
+class Test_login:
+    """
+    GitHub GUI测试的运行
+    """
+    data_list = Data_Base("login").result
+
+    def setup_class(self):
+        # 初始化浏览器实例并且打开被测页面
+        self.d = Case_Login()
+        self.d.start_log()
+        self.d.start_operation("http://jfzy.eastedu.test/#/login")
+
+    def teardown_class(self):
+        self.d.close_operation()
+
+    @pytest.mark.run(order=1)
+    @pytest.mark.parametrize(argnames="username", argvalues=data_list["e_username"])
+    @pytest.mark.parametrize(argnames="passwd", argvalues=data_list["e_passwd"])
+    def test_error_login(self, username, passwd, case_name="login_error"):
+        self.d.username_input_input(case_name, username)
+        self.d.passwd_input_input(case_name, passwd)
+        self.d.signup_button_click(case_name)
+
+        # 错误断言
+        self.d.errorlogin_text_assertion(case_name)
+
+    @pytest.mark.run(order=2)
+    @pytest.mark.parametrize(argnames="username", argvalues=data_list["c_username"])
+    @pytest.mark.parametrize(argnames="passwd", argvalues=data_list["c_passwd"])
+    def test_correct_login(self, username, passwd, case_name="login_correct"):
+        self.d.username_input_input(case_name, username)
+        self.d.passwd_input_input(case_name, passwd)
+        self.d.signup_button_click(case_name)
+
+        # 正确断言
+        self.d.userinfo_text_assertion(case_name)
