0615311ef2d7b7f59ec56df4fbaa4ede46affbdd
sabersame <38524798+sabersame@users.noreply.github.com>
2020-01-16 14:14:44 +0800

    Update case_login.py

--- login/case_login.py
+++ login/case_login.py
@@ -14,21 +14,21 @@ class Case_Login(Base_Base):
 
     def username_input_input(self, case_name, username):
         # 用户名
-        self.input(case_name, 'xpath', '//*[@id="app-el"]/div/div/div/div[2]/div/div[2]/input', username)
+        self._input(case_name, 'xpath', '//*[@id="app-el"]/div/div/div/div[2]/div/div[2]/input', username)
 
     def passwd_input_input(self, case_name, passwd):
         # 密码
-        self.input(case_name, 'xpath', '//*[@id="app-el"]/div/div/div/div[2]/div/div[3]/input', passwd)
+        self._input(case_name, 'xpath', '//*[@id="app-el"]/div/div/div/div[2]/div/div[3]/input', passwd)
 
     def signup_button_click(self, case_name):
         # 登陆按钮
-        self.click(case_name, 'xpath', '//*[@id="app-el"]/div/div/div/div[2]/div/div[4]/button')
+        self._click(case_name, 'xpath', '//*[@id="app-el"]/div/div/div/div[2]/div/div[4]/button')
 
     def userinfo_text_assertion(self, case_name):
-        self.assertion(case_name, 'xpath', '//*[@id="app-el"]/div/div/div/div/div[1]/div[3]/div/div/div', "狗狗")
+        self._assertion(case_name, 'xpath', '//*[@id="app-el"]/div/div/div/div/div[1]/div[3]/div/div/div', "狗狗")
 
     def errorlogin_text_assertion(self, case_name):
-        self.assertion(case_name, 'xpath', '//*[@id="app-el"]/div/div/div/div[2]/div/div[4]/div', '密码错误!')
+        self._assertion(case_name, 'xpath', '//*[@id="app-el"]/div/div/div/div[2]/div/div[4]/div', '密码错误!')
 
     # 启动时日志记录
     def start_log(self):
@@ -40,8 +40,8 @@ class Case_Login(Base_Base):
 
     def sleep_operation(self, case_name):
         # 暂停操作
-        self.delay(case_name)
+        self._delay(case_name)
 
     def close_operation(self):
         # 关闭浏览器
-        self.quit()
+        self._quit()
